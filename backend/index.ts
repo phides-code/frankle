580c55e79fbd4373eb6696bf4036fcc242bad243
Phil DeSousa <phil.desousa@gmail.com>
2022-12-12 12:12:21 -0500

    updates for render deployment

--- backend/index.ts
+++ backend/index.ts
@@ -2,19 +2,23 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import getWord from './handlers/getWord';
 import getAllWords from './handlers/getAllWords';
+import path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
 
 app.get('/api/getword', getWord);
 app.get('/api/getallwords', getAllWords);
 
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, '../../frontend/build', 'index.html'));
+});
+
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    console.log(`*** Server is running on port ${port} ***`);
 });
